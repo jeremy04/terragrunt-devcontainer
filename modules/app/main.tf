b02eb8efe2b02449eaaa5d3c481005c03fafa7c1
Jeremy Lipson <jeremy04@gmail.com>
2025-06-30 22:02:11 -0400

    chore: terragrunt format

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,28 +1,28 @@
 
 module "app_alb" {
-  source = "terraform-aws-modules/alb/aws"
-  name   = "app-alb"
-  subnets = var.public_subnets
+  source          = "terraform-aws-modules/alb/aws"
+  name            = "app-alb"
+  subnets         = var.public_subnets
   security_groups = [var.alb_sg]
-  vpc_id = var.vpc_id
+  vpc_id          = var.vpc_id
 }
 
 module "ecs_app" {
   source = "terraform-aws-modules/ecs/aws"
 
-  name = "rails-api"
-  launch_type = "FARGATE"
+  name          = "rails-api"
+  launch_type   = "FARGATE"
   desired_count = 2
 
   container_definitions = jsonencode([
     {
-      name = "nginx"
-      image = "nginx:latest"
+      name         = "nginx"
+      image        = "nginx:latest"
       portMappings = [{ containerPort = 443 }]
     },
     {
-      name = "rails"
-      image = "myorg/rails-api:latest"
+      name         = "rails"
+      image        = "myorg/rails-api:latest"
       portMappings = [{ containerPort = 3000 }]
     }
   ])
@@ -31,14 +31,14 @@ module "ecs_app" {
 module "ecs_sidekiq" {
   source = "terraform-aws-modules/ecs/aws"
 
-  name = "sidekiq"
-  launch_type = "FARGATE"
+  name          = "sidekiq"
+  launch_type   = "FARGATE"
   desired_count = 1
 
   container_definitions = jsonencode([
     {
-      name = "sidekiq"
-      image = "myorg/rails-api:latest"
+      name    = "sidekiq"
+      image   = "myorg/rails-api:latest"
       command = ["bundle", "exec", "sidekiq"]
     }
   ])
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,22 +1,22 @@
 
 module "rds" {
-  source  = "terraform-aws-modules/rds/aws"
+  source = "terraform-aws-modules/rds/aws"
 
-  identifier = "app-db"
-  engine     = "postgres"
-  instance_class = "db.t3.medium"
+  identifier        = "app-db"
+  engine            = "postgres"
+  instance_class    = "db.t3.medium"
   allocated_storage = 20
 
   vpc_security_group_ids = [var.db_sg]
   subnet_ids             = var.private_subnets
 
-  backup_window   = "03:00-04:00"
+  backup_window           = "03:00-04:00"
   backup_retention_period = 7
-  maintenance_window = "sun:04:00-sun:05:00"
+  maintenance_window      = "sun:04:00-sun:05:00"
 
-  create_db_subnet_group = true
-  create_monitoring_role = true
-  create_random_password = false
-  manage_master_user_password = true
+  create_db_subnet_group        = true
+  create_monitoring_role        = true
+  create_random_password        = false
+  manage_master_user_password   = true
   master_user_secret_kms_key_id = var.kms_key_id
 }
